30b29f6061d6913438cf97efccb5267eef10291a
ernesto2011 <skillmx107@gmail.com>
2025-04-12 21:39:11 -0600

    :rocekt: :rocket: inciando proyecto con react-router-dom v7

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+//import { AuthLayout } from "@/auth/layout/AuthLayout"
+
+import ChatLayout from "./chat/layout/ChatLayout"
+import ChatPage from "./chat/pages/ChatPage"
+
+
+function App() {
+
+  return (
+<>
+{/* <AuthLayout/> */}
+<ChatLayout>
+  <ChatPage/>
+</ChatLayout>
+</>
+  )
+}
+
+export default App
